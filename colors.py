096d62c3cd4acb2c8e520c312aecf4c14e6d4a3b
sbenchik <stevebenchik@gmail.com>
2016-05-06 22:21:42 -0400

    Added dark mode for plain text lexer only Refactored editor class into its own file

--- colors.py
+++
@@ -1,7 +0,0 @@
-import sys
-from PyQt4 import QtCore, QtGui, Qsci
-from PyQt4.QtGui import *
-
-class algol(Qsci.QsciLexerCustom):
-    from writerqsci import mainWindow
-    config.m.edit.lexer.setColor('#888888', 1)
